e7dd1bc6fb45a7bca65f1e84d49893e4df2de031
Nisarga <your_email@gmail.com>
2026-06-17 21:22:46 +0530

    Initialized the file

---
+++ hoisting.js
@@ -0,0 +1,49 @@
+// console.log(a);
+
+// var a = 10;
+
+// console.log(a);
+
+
+
+// console.log(b);
+
+// let b = 20;
+
+// console.log(b);
+
+
+
+// console.log(c);
+
+// const c = 30;
+
+// console.log(c);
+
+
+
+console.log(x); // undefined
+//console.log(y); // ReferenceError
+//console.log(z); // ReferenceError
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+
+
+console.log(x); // undefined
+console.log(y); // ReferenceError
+console.log(z); // ReferenceError
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x);
+console.log(y);
+console.log(z);
